858d72c3006ac6fa090d95250f5004eb6e4099ee
Jason Kraus <jasonk@cukerinteractive.com>
2011-06-08 13:07:31 -0700

    added comment

--- examples/spawn_subsites.py
+++ examples/spawn_subsites.py
@@ -6,3 +6,6 @@ application.create_subapp('/site1/', 'settings.site1_settings')
 application.create_subapp('/site2/', 'settings.site2_settings')
 application.create_subapp('/', 'settings.base_settings')
 
+#Note: this assumes gunicorn and django, make sure gunicorn is in your installed apps
+#Example usage: bin/gunicorn spawn_subsites:application
+
